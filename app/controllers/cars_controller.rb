f01d52bf67fc7a340fd6cd768c32bea0dc9c2c1d
cassaram09 <cassaram09@gmail.com>
2016-05-24 17:18:54 -0400

    fixed adding new cars functionality, added car show page, added list of cars to dashboard

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -17,16 +17,19 @@ class CarsController < ApplicationController
     @user = User.find_by_slug(params[:slug])
     @current = Helpers.current_user(session)
     if @current.id = @user.id
-      erb :'/users/cars/index'
+      @car = Car.find_by(id: params[:id])
+      erb :'/users/cars/show'
     else
       redirect "/users/#{@current.slug}"
     end
   end
 
   post '/users/:slug/cars' do
+    binding.pry
     @user = Helpers.current_user(session)
-    @game = Game.create(date: params[:date])
-    
+    @car = Car.create(params[:car])
+    @car.update(user_id: @user.id)
+    redirect "/users/#{@user.slug}/cars/#{@car.id}"
   end
 
   get '/users/:slug/cars/:id' do
